bc01f4f1bdabb0f19b9b5c766d580c054899fc76
BrunoSDomingues <brunao.signorelli@hotmail.com>
2020-03-09 20:18:28 -0300

    Feita função play e toca uma música com botão

--- src/main.c
+++ src/main.c
@@ -13,17 +13,23 @@
 /* defines                                                              */
 /************************************************************************/
 
-// Configuracoes do LED
+// Configuracoes do Buzzer
 #define BUZ_PIO           PIOC                 // periferico que controla o Buzzer
 #define BUZ_PIO_ID        ID_PIOC                  // ID do periférico PIOC (controla Buzzer)
 #define BUZ_PIO_IDX       13                    // ID do Buzzer no PIO
 #define BUZ_PIO_IDX_MASK  (1u << BUZ_PIO_IDX)   // Mascara para CONTROLARMOS o Buzzer
 
-// Configuracoes do botao
-#define BUT_PIO			  PIOA
-#define BUT_PIO_ID		  ID_PIOA
-#define BUT_PIO_IDX		  19
-#define BUT_PIO_IDX_MASK  (1u << BUT_PIO_IDX)
+// Configuracoes do botao play
+#define PLAY_PIO			PIOA
+#define PLAY_PIO_ID			ID_PIOA
+#define PLAY_PIO_IDX		19
+#define PLAY_PIO_IDX_MASK	(1u << PLAY_PIO_IDX)
+
+// Configuracoes do botao troca musica
+//#define NEXT_PIO			PIOB
+//#define NEXT_PIO_ID			ID_PIOB
+//#define NEXT_PIO_IDX		2
+//#define NEXT_PIO_IDX_MASK	(1u << NEXT_PIO_IDX)
 
 // Notas musicais
 
@@ -121,7 +127,7 @@
 /* constants                                                            */
 /************************************************************************/
 
-int melody[] = {
+const int s1[] = {
 	NOTE_E7, NOTE_E7, 0, NOTE_E7,
 	0, NOTE_C7, NOTE_E7, 0,
 	NOTE_G7, 0, 0,  0,
@@ -145,10 +151,9 @@ int melody[] = {
 	NOTE_G6, NOTE_E7, NOTE_G7,
 	NOTE_A7, 0, NOTE_F7, NOTE_G7,
 	0, NOTE_E7, 0, NOTE_C7,
-	NOTE_D7, NOTE_B6, 0, 0
+	NOTE_D7, NOTE_B6
 };
-
-int tempo[] = {
+const int t1[] = {
 	12, 12, 12, 12,
 	12, 12, 12, 12,
 	12, 12, 12, 12,
@@ -172,8 +177,53 @@ int tempo[] = {
 	9, 9, 9,
 	12, 12, 12, 12,
 	12, 12, 12, 12,
-	12, 12, 12, 12,
+	12, 12
 };
+
+/* const int s2[] = {
+	NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+	NOTE_AS3, NOTE_AS4, 0,
+	0,
+	NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+	NOTE_AS3, NOTE_AS4, 0,
+	0,
+	NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+	NOTE_DS3, NOTE_DS4, 0,
+	0,
+	NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+	NOTE_DS3, NOTE_DS4, 0,
+	0, NOTE_DS4, NOTE_CS4, NOTE_D4,
+	NOTE_CS4, NOTE_DS4,
+	NOTE_DS4, NOTE_GS3,
+	NOTE_G3, NOTE_CS4,
+	NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+	NOTE_GS4, NOTE_DS4, NOTE_B3,
+	NOTE_AS3, NOTE_A3, NOTE_GS3,
+	0, 0, 0
+};
+const int t2[] = {
+	12, 12, 12, 12,
+	12, 12, 6,
+	3,
+	12, 12, 12, 12,
+	12, 12, 6,
+	3,
+	12, 12, 12, 12,
+	12, 12, 6,
+	3,
+	12, 12, 12, 12,
+	12, 12, 6,
+	6, 18, 18, 18,
+	6, 6,
+	6, 6,
+	6, 6,
+	18, 18, 18, 18, 18, 18,
+	10, 10, 10,
+	10, 10, 10,
+	3, 3, 3
+};
+*/
+
 
 /************************************************************************/
 /* variaveis globais                                                    */
@@ -185,6 +235,7 @@ int tempo[] = {
 
 void init(void);
 void tone(int freq, int dur);
+void play(int note, int tempo, int compass);
 
 /************************************************************************/
 /* interrupcoes                                                         */
@@ -210,12 +261,15 @@ void init(void){
 	pio_set_output(BUZ_PIO, BUZ_PIO_IDX_MASK, 0, 0, 0);
 	
 	// Inicializa PIO do botao
-	pmc_enable_periph_clk(BUT_PIO_ID);
+	pmc_enable_periph_clk(PLAY_PIO_ID);
+	//pmc_enable_periph_clk(NEXT_PIO_ID);
 	
 	// configura pino ligado ao botão como entrada com um pull-up.
-	pio_set_input(BUT_PIO_ID, BUT_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_set_input(PLAY_PIO_ID, PLAY_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_pull_up(PLAY_PIO_ID, PLAY_PIO_IDX_MASK, 1);
 	
-	pio_pull_up(BUT_PIO_ID, BUT_PIO_IDX_MASK, 1);
+	// pio_set_input(NEXT_PIO_ID, NEXT_PIO_IDX_MASK, PIO_DEFAULT);
+	// pio_pull_up(NEXT_PIO_ID, NEXT_PIO_IDX_MASK, 1);
 }
 
 void tone(int freq, int dur){
@@ -233,8 +287,14 @@ void tone(int freq, int dur){
 	}
 }
 
-void play(int song[], int tempo[], int compass){
-	
+void play(int note, int tempo, int compass){
+	int noteDuration = compass/tempo;
+		
+	tone(note, noteDuration);
+	int pauseBetweenNotes = noteDuration * 1.30;
+	delay_ms(pauseBetweenNotes);
+		
+	tone(0, noteDuration);
 }
 
 /************************************************************************/
@@ -242,25 +302,15 @@ void play(int song[], int tempo[], int compass){
 /************************************************************************/
 
 // Funcao principal chamada na inicalizacao do uC.
-int main(void)
-{
+int main(void){
 	// inicializa sistema e IOs
 	init();
-
 	// super loop
 	// aplicacoes embarcadas não devem sair do while(1).
-	while (1)
-	{
-		if (pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK) == 0){
-			int size = sizeof(melody)/sizeof(int);
-			for (int thisNote = 0; thisNote < size; thisNote++){
-				int noteDuration = 1000/tempo[thisNote];
-				
-				tone(melody[thisNote],noteDuration);
-				int pauseBetweenNotes = noteDuration * 1.30;
-				delay_ms(pauseBetweenNotes);
-				
-				tone(0, noteDuration);
+	while (1){		
+		if (pio_get(PLAY_PIO, PIO_INPUT, PLAY_PIO_IDX_MASK) == 0){
+			for (int i = 0; i < sizeof(s1); i++){
+				play(s1[i],t1[i],700);
 			}
 		}
 	}
