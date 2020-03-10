1a4607aefe6a0295f71e5d9aaf37c4c59120e487
BrunoSDomingues <brunao.signorelli@hotmail.com>
2020-03-10 15:37:41 -0300

    Play de duas músicas parcialmente funcional

--- src/main.c
+++ src/main.c
@@ -8,10 +8,17 @@
 /************************************************************************/
 
 #include "asf.h"
+#include "notas.h"
 
 /************************************************************************/
 /* defines                                                              */
 /************************************************************************/
+
+// Configuracoes do Led
+#define LED_PIO           PIOC                 // periferico que controla o LED
+#define LED_PIO_ID        12                  // ID do periférico PIOC (controla LED)
+#define LED_PIO_IDX       8                    // ID do LED no PIO
+#define LED_PIO_IDX_MASK  (1 << LED_PIO_IDX)   // Mascara para CONTROLARMOS o LED
 
 // Configuracoes do Buzzer
 #define BUZ_PIO           PIOC                 // periferico que controla o Buzzer
@@ -26,102 +33,10 @@
 #define PLAY_PIO_IDX_MASK	(1u << PLAY_PIO_IDX)
 
 // Configuracoes do botao troca musica
-//#define NEXT_PIO			PIOB
-//#define NEXT_PIO_ID			ID_PIOB
-//#define NEXT_PIO_IDX		2
-//#define NEXT_PIO_IDX_MASK	(1u << NEXT_PIO_IDX)
-
-// Notas musicais
-
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
+#define NEXT_PIO			PIOB
+#define NEXT_PIO_ID			ID_PIOB
+#define NEXT_PIO_IDX		2
+#define NEXT_PIO_IDX_MASK	(1u << NEXT_PIO_IDX)
 
 /************************************************************************/
 /* constants                                                            */
@@ -180,7 +95,7 @@ const int t1[] = {
 	12, 12
 };
 
-/* const int s2[] = {
+const int s2[] = {
 	NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
 	NOTE_AS3, NOTE_AS4, 0,
 	0,
@@ -222,7 +137,6 @@ const int t2[] = {
 	10, 10, 10,
 	3, 3, 3
 };
-*/
 
 
 /************************************************************************/
@@ -254,22 +168,24 @@ void init(void){
 	WDT->WDT_MR = WDT_MR_WDDIS;
 	
 	// Ativa o PIO na qual o LED foi conectado
-	// para que possamos controlar o LED.
+	// para que possamos controlar o LED e os botões
+	pmc_enable_periph_clk(LED_PIO_ID);
 	pmc_enable_periph_clk(BUZ_PIO_ID);
 	
 	//Inicializa PC8 como saída
+	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
 	pio_set_output(BUZ_PIO, BUZ_PIO_IDX_MASK, 0, 0, 0);
 	
 	// Inicializa PIO do botao
 	pmc_enable_periph_clk(PLAY_PIO_ID);
-	//pmc_enable_periph_clk(NEXT_PIO_ID);
+	pmc_enable_periph_clk(NEXT_PIO_ID);
 	
 	// configura pino ligado ao botão como entrada com um pull-up.
 	pio_set_input(PLAY_PIO_ID, PLAY_PIO_IDX_MASK, PIO_DEFAULT);
 	pio_pull_up(PLAY_PIO_ID, PLAY_PIO_IDX_MASK, 1);
 	
-	// pio_set_input(NEXT_PIO_ID, NEXT_PIO_IDX_MASK, PIO_DEFAULT);
-	// pio_pull_up(NEXT_PIO_ID, NEXT_PIO_IDX_MASK, 1);
+	pio_set_input(NEXT_PIO_ID, NEXT_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_pull_up(NEXT_PIO_ID, NEXT_PIO_IDX_MASK, 1);
 }
 
 void tone(int freq, int dur){
@@ -280,9 +196,11 @@ void tone(int freq, int dur){
 	// x = freq/1000
 	int j = (dur * freq)/1000;
 	for(int i = 0; i <= j; i++){
-		pio_set(PIOC, BUZ_PIO_IDX_MASK);      // Coloca 1 no pino LED
-		delay_us(t);                        // Delay por software de 200 ms
-		pio_clear(PIOC, BUZ_PIO_IDX_MASK);    // Coloca 0 no pino do LED
+		pio_set(PIOC, LED_PIO_IDX_MASK);	// Acende o LED
+		pio_set(PIOC, BUZ_PIO_IDX_MASK);      // Coloca som no buzzer
+		delay_us(t);                        // Delay por software de t us
+		pio_clear(PIOC, LED_PIO_IDX_MASK);	// Apaga o LED
+		pio_clear(PIOC, BUZ_PIO_IDX_MASK);    // Tira som do buzzer
 		delay_us(t);
 	}
 }
@@ -302,15 +220,41 @@ void play(int note, int tempo, int compass){
 /************************************************************************/
 
 // Funcao principal chamada na inicalizacao do uC.
+typedef struct {
+	int* songs[2];
+	int* tempos[2];
+	int lengths[2];
+} musica;
+
 int main(void){
 	// inicializa sistema e IOs
 	init();
+	
+	musica m;
+	m.songs[0] = &s1;
+	m.songs[1] = &s2;
+	m.lengths[0] = sizeof(s1)/sizeof(int);
+	m.lengths[1] = sizeof(s2)/sizeof(int);
+	m.tempos[0] = &t1;
+	m.tempos[1] = &t2;
+	int choice = 0;
+
 	// super loop
 	// aplicacoes embarcadas não devem sair do while(1).
-	while (1){		
+	while (1){
 		if (pio_get(PLAY_PIO, PIO_INPUT, PLAY_PIO_IDX_MASK) == 0){
-			for (int i = 0; i < sizeof(s1); i++){
-				play(s1[i],t1[i],700);
+			
+			musica:;
+			int len = m.lengths[choice];
+			int* s = m.songs[choice];
+			int* t = m.tempos[choice];
+			for (int i = 0; i < len; i++){
+				if (pio_get(NEXT_PIO, PIO_INPUT, NEXT_PIO_IDX_MASK) == 0){
+					choice = !choice;
+					delay_ms(300);
+					goto musica;
+				}
+				play(s[i],t[i],800);
 			}
 		}
 	}
